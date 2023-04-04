168f4b24cf68201e7b4b0012ec06af2e11a1ca6b
jhonc17 <jhon.henry.urbina.17@gmail.com>
2023-04-04 16:09:07 -0400

    change simulator chain image (homepage)

--- App/Components/Home/TopChainsCard.js
+++ App/Components/Home/TopChainsCard.js
@@ -85,11 +85,7 @@ const TopChainsCard = ({ topChains }) => {
               <View style={styles.imageContainer}>
                 <Image
                   style={styles.gameImage}
-                  source={
-                    I18n.locale === "es"
-                      ? require("../../Assets/Images/chain_simulator.png")
-                      : require("../../Assets/Images/chain_simulator.png")
-                  }
+                  source={require("../../Assets/Images/chain_simulator.png")}
                 />
               </View>
             </View>
