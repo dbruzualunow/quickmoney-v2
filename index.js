a0f0461668644a21af611afef3853d3b169475c7
jhonc17 <jhon.henry.urbina.17@gmail.com>
2023-06-27 14:16:48 -0400

    revert old reminder plays per days

--- index.js
+++ index.js
@@ -8,21 +8,18 @@ import {name as appName} from './app.json';
 import PushNotification , {Importance} from 'react-native-push-notification';
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-
 PushNotification.createChannel(
     {
-      channelId: "channelId", // (required)
-      channelName: "My channel", // (required)
-      channelDescription: "A channel to categorise your notifications", // (optional) default: undefined.
-      playSound: true, // (optional) default: true
-      importance: Importance.HIGH, // (optional) default: Importance.HIGH. Int value of the Android notification importance
-      vibrate: true, // (optional) default: true. Creates the default vibration pattern if true.
+      channelId: "channelId",
+      channelName: "My channel",
+      channelDescription: "A channel to categorise your notifications",
+      playSound: true,
+      importance: Importance.HIGH,
+      vibrate: true,
     },
-    (created) => console.log(`createChannel returned '${created}'`) // (optional) callback returns whether the channel was created, false means it already existed.
+    (created) => console.log(`createChannel returned '${created}'`)
 );
 PushNotification.configure({
-
-  // (required) Called when a remote or local notification is opened or received
   onNotification: function(notification) {
     console.log('REMOTE NOTIFICATION ==>', notification)
     if (notification.foreground) {
@@ -30,24 +27,15 @@ PushNotification.configure({
         title: notification.title,
         message: notification.message,
         channelId: 'channelId',
-        // largeIcon: "ic_notification",
-        // smallIcon: "ic_notification",
-        // bigLargeIcon: "ic_notification"
       })
     }
-    // process the notification here
   },
   onRegister: function(token) {
     console.log('TOKEN:', token)
     AsyncStorage.setItem('fcmToken', token?.token || '')
   },
-  // Android only: GCM or FCM Sender ID
-  // senderID: "393809536636",
   popInitialNotification: true,
   requestPermissions: true,
-  // largeIcon: "ic_launcher_foreground",
-  // smallIcon: "ic_launcher_foreground",
-  // bigLargeIcon: "ic_launcher_foreground",
 })
 
 AppRegistry.registerComponent(appName, () => App);
